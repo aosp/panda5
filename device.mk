76b16f411d229287c995891f80b914134a39c3a8
Kevin McCray <kevin.mccray@ti.com>
2012-09-26 10:11:55 -0700

    HACK: Panda5: Increase dalvik heap size
    We have seen ANR's caused by out of memory errors in the dalvik heap.
    I've been researching the right heap size to use for this board but
    so far have not found a lot of information out there on how to
    calculate the proper size given different board parameters (DDR size,
    screen resolution, etc...).
    
    So, for now I am going to increase it to the max pre-configured heap
    size available on Android and we'll continue to investigate whether
    an additional increase will be needed.
    
    Change-Id: Ice7ca864576321e816f467ca371b19a4a591c9c5
    Signed-off-by: Kevin McCray <kevin.mccray@ti.com>

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
 	hciconfig \
 	hcitool
 
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/ti/omap4xxx/omap5.mk)
 $(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, vendor/ti/omap5sevm/device-vendor.mk)
